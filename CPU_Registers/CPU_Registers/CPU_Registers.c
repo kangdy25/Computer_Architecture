04e1f95d2cca92a6ea45c94b504e8d8e07c2d246
Kangdy <canfrog@naver.com>
2023-02-07 00:00:25 +0900

    Learn CPU Register

---
+++ CPU_Registers/CPU_Registers/CPU_Registers.c
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+#include <time.h>
+
+int main() {
+	register int i, j;
+	clock_t begin, end;
+	double time_spent;
+
+	begin = clock();
+	for (i = 0; i < 10000; i++) {
+		for (j = 0; j < 10000; j++) {
+
+		}
+	}
+	end = clock();
+	time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+	printf("with register: time spent %f\n", time_spent);
+	
+	int a, b, temp2;
+	begin = clock();
+	for (a = 0; a < 10000; a++) {
+		for (b = 0; b < 10000; b++) {
+
+		}
+	}
+	end = clock;
+	time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+	printf("without register: time spent %f\n", time_spent);
+	getchar();
+	return 0;
+}
